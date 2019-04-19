683b92ac6d2da0ed91d19b8b70ea1df1f2819c2e
unknown <adrianvpl@gmail.com>
2019-04-19 18:18:05 +0200

    add full func

--- EasyAntiCheat.sys/vm.asm
+++ EasyAntiCheat.sys/vm.asm
@@ -1,7 +1,35 @@
+.text:0000000000036C30                         ExecVMREAD      proc near               ; CODE XREF: CheckVM+21↑p
+.text:0000000000036C30 0F 78 0A                                vmread  qword ptr [rdx], rcx
+.text:0000000000036C33 0F 94 C0                                setz    al
+.text:0000000000036C36 0F 92 C1                                setb    cl
+.text:0000000000036C39 12 C1                                   adc     al, cl
+.text:0000000000036C3B C3                                      retn
+.text:0000000000036C3B                         ExecVMREAD      endp
+
+.text:000000000001FD84                         CheckVM         proc near               ; DATA XREF: .pdata:000000000004F960↓o
+.text:000000000001FD84
+.text:000000000001FD84                         var_28          = byte ptr -28h
+.text:000000000001FD84                         var_18          = dword ptr -18h
+.text:000000000001FD84                         var_14          = dword ptr -14h
+.text:000000000001FD84                         var_10          = dword ptr -10h
+.text:000000000001FD84
+.text:000000000001FD84                         ; FUNCTION CHUNK AT .text:000000000003724A SIZE 00000023 BYTES
+.text:000000000001FD84
+.text:000000000001FD84                         ; __unwind { // __C_specific_handler
+.text:000000000001FD84 40 53                                   push    rbx
+.text:000000000001FD86 48 83 EC 40                             sub     rsp, 40h
+.text:000000000001FD8A B3 01                                   mov     bl, 1
+.text:000000000001FD8C 88 5C 24 20                             mov     [rsp+48h+var_28], bl
+.text:000000000001FD90 33 C0                                   xor     eax, eax
+.text:000000000001FD92 89 44 24 30                             mov     [rsp+48h+var_18], eax
+.text:000000000001FD96 89 44 24 34                             mov     [rsp+48h+var_14], eax
+.text:000000000001FD9A 89 44 24 38                             mov     [rsp+48h+var_10], eax
+.text:000000000001FD9E
+.text:000000000001FD9E                         loc_1FD9E:                              ; DATA XREF: .rdata:0000000000049114↓o
 .text:000000000001FD9E                         ;   __try { // __except at VMNotFound
 .text:000000000001FD9E 48 8D 54 24 34                          lea     rdx, [rsp+48h+var_14]
 .text:000000000001FDA3 33 C9                                   xor     ecx, ecx
-.text:000000000001FDA5 E8 86 6E 01 00                          call    CheckVM
+.text:000000000001FDA5 E8 86 6E 01 00                          call    ExecVMREAD
 .text:000000000001FDAA 88 5C 24 20                             mov     [rsp+48h+var_28], bl
 .text:000000000001FDAE EB 16                                   jmp     short VMFound
 .text:000000000001FDAE                         ;   } // starts at 1FD9E
@@ -15,7 +43,7 @@
 .text:000000000001FDBF 0F 44 D8                                cmovz   ebx, eax
 .text:000000000001FDC2 88 5C 24 20                             mov     [rsp+48h+var_28], bl
 .text:000000000001FDC6
-.text:000000000001FDC6                         VMFound:                                ; CODE XREF: sub_1FD84+2A↑j
+.text:000000000001FDC6                         VMFound:                                ; CODE XREF: CheckVM+2A↑j
 .text:000000000001FDC6 F6 DB                                   neg     bl
 .text:000000000001FDC8 1B C9                                   sbb     ecx, ecx
 .text:000000000001FDCA 81 C1 55 01 00 00                       add     ecx, 155h
@@ -27,11 +55,4 @@
 .text:000000000001FDE5 5B                                      pop     rbx
 .text:000000000001FDE6 C3                                      retn
 .text:000000000001FDE6                         ; } // starts at 1FD84
-
-.text:0000000000036C30                         CheckVM         proc near               ; CODE XREF: sub_1FD84+21↑p
-.text:0000000000036C30 0F 78 0A                                vmread  qword ptr [rdx], rcx
-.text:0000000000036C33 0F 94 C0                                setz    al
-.text:0000000000036C36 0F 92 C1                                setb    cl
-.text:0000000000036C39 12 C1                                   adc     al, cl
-.text:0000000000036C3B C3                                      retn
-.text:0000000000036C3B                         CheckVM         endp
+.text:000000000001FDE6                         CheckVM         endp
